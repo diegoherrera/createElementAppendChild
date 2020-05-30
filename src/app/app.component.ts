f35e6e4703ff336727927289e69d2149a0052004
Diego Sebastian Herrera <diherrer@everis.com>
2020-05-30 12:03:55 -0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild
+  , ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'render2';
+  @ViewChild('contenedor')
+  private contenedorPadre: ElementRef;  
+  constructor(private rendered: Renderer2) { }
+
+  onClickEvent() {
+    //primero vamos a crear un element li
+    let li = this.rendered.createElement('li');
+    //creo un element texto
+    let texto = this.rendered.createText("Mensaje dentro del li");
+    //vinculo el texto con el li 
+    this.rendered.appendChild(li, texto);
+    //vinculo el nuevo elmento creado con el contenedor 
+    this.rendered.appendChild(this.contenedorPadre.nativeElement, li);
+
+  }
 }
